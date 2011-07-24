663772aa48d70652a726a7ef1e3b39dd83f88511
Flaviu Simihaian <flsimihaian@gmail.com>
2011-07-23 22:00:36 -0400

    Added helped and stylesheets

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
   # Return a title on a per-page basis.
   def title
-    base_title = "Rails rock App"
+    base_title = "Rails Rocks"
     if @title.nil?
       base_title
     else
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -3,6 +3,10 @@ require 'spec_helper'
 describe PagesController do
   render_views
 
+  before(:each) do
+    @base_title = "Rails Rocks"
+  end
+
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
@@ -13,7 +17,7 @@ describe PagesController do
   it "should have the right title" do
     get 'home'
     response.should have_selector("title", 
-                                  :content => "Rails rock App | Home")
+                                  :content => "#{@base_title} | Home")
   end
 
   it "should have a non-blank body" do
@@ -30,7 +34,7 @@ describe PagesController do
     it "should have the right title" do
       get 'contact'
       response.should have_selector("title", 
-                                    :content => "Rails rock App | Contact")
+                                    :content => "#{@base_title} | Contact")
     end
   end
 
@@ -43,7 +47,7 @@ describe PagesController do
     it "should have the right title" do
       get 'about'
       response.should have_selector("title", 
-                                    :content => "Rails rock App | About")
+                                    :content => "#{@base_title} | About")
     end
   end
 
