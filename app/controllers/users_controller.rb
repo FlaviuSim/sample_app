615c44eb569a6bc4e27d2e00c1edadb20a8dfcdf
Flaviu Simihaian <flsimihaian@gmail.com>
2011-07-28 09:05:48 -0400

    added editing and updating user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,10 +15,26 @@ class UsersController < ApplicationController
     @user = User.new(params[:user])
     if @user.save
       sign_in @user
-      redirect_to @user, :flash => { :success => "Welcome to the sample app" }
+      redirect_to @user, :flash => { :success => "Welcome to the sample app!" }
     else
       @title = "Sign up"
       render 'new'
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+    @title = "Edit user"
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      redirect_to @user,:flash => { :success => "Profile updated." }
+ 
+    else
+      @title = "Edit user"
+      render 'edit'
     end
   end
 
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -23,8 +23,8 @@ describe "Users" do
       it "should should make a new user" do
         lambda do
           visit signup_path
-          fill_in "Name",           :with => "Example User"
-          fill_in "Email",          :with => "user@example.com"
+          fill_in "Name",             :with => "Example User"
+          fill_in "Email",            :with => "user@example.com"
           fill_in "Password",        :with => "foobar"
           fill_in "Confirmation",    :with => "foobar"
           click_button
