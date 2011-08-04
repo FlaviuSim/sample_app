d1a0c5fa5985a6d8f1629579d61fbb09ce78402f
Flaviu Simihaian <flsimihaian@gmail.com>
2011-08-04 14:38:09 -0400

    Added ability to delete users (if admin)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
-  before_filter :authenticate, :only => [:index, :edit, :update]
+  before_filter :authenticate, :only => [:index, :destroy, :edit, :update]
   before_filter :correct_user, :only => [:edit, :update]
+  before_filter :admin_user,   :only => :destroy
 
   def index
     @users = User.paginate(:page => params[:page])
@@ -29,12 +30,10 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
     @title = "Edit user"
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update_attributes(params[:user])
       redirect_to @user,:flash => { :success => "Profile updated." }
  
@@ -42,6 +41,11 @@ class UsersController < ApplicationController
       @title = "Edit user"
       render 'edit'
     end
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    redirect_to users_path, :flash => { :success => "User destroyed." }
   end
 
   private
@@ -53,5 +57,10 @@ class UsersController < ApplicationController
   def correct_user
     @user = User.find(params[:id])
     redirect_to(root_path) unless current_user?(@user)
+  end
+
+  def admin_user
+    user = User.find(params[:id])
+    redirect_to(root_path, :flash => { :error => "Nope." }) if !current_user.admin? || current_user?(user)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -56,6 +56,7 @@ end
 
 
 
+
 # == Schema Information
 #
 # Table name: users
@@ -67,5 +68,6 @@ end
 #  updated_at         :datetime
 #  encrypted_password :string(255)
 #  salt               :string(255)
+#  admin              :boolean         default(FALSE)
 #
 
